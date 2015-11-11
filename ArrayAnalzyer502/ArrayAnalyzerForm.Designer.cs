ffd0387b2378b2171b15f095c1b5680651300cb0
pirates19 <nicholas.ellul1@gmail.com>
2015-11-11 14:46:38 -0500

    Not done

---
+++ ArrayAnalzyer502/ArrayAnalyzerForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ArrayAnalzyer502
+{
+    partial class frmArray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(96, 174);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "button1";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "frmArray";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ ArrayAnalzyer502/ArrayAnalyzerForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayAnalzyer502
+{
+    public partial class frmArray : Form
+    {
+        int[] arrayOfNumbers = new int[10];
+        Random randomNumbers = new Random();
+
+        public int BiggestNumber(int[] array)
+        {
+            //returns biggest number in array
+            int elementHolder = 0;
+            foreach(int element in array)
+            {
+                if(elementHolder < element)
+                {
+                    elementHolder = element;
+                }
+              
+            }
+            return elementHolder;
+        }
+
+        public frmArray()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            const int NUMBER_OF_ITEMS = 10;
+            int randomizedNumber;
+            int greatestNumber;
+          
+            //process
+            for (int counter = 0; counter < NUMBER_OF_ITEMS; counter++)
+            {
+                randomizedNumber = randomNumbers.Next(1, 10 + 1);
+                arrayOfNumbers[counter] = randomizedNumber;
+            }
+
+            //output
+            greatestNumber = BiggestNumber(arrayOfNumbers);
+
+            MessageBox.Show(Convert.ToString(greatestNumber));
+           
+        }
+    }
+}
