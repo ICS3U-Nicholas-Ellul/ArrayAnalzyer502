0a7052d9fdb4c40f4e29b0c974a20f72696d2244
pirates19 <nicholas.ellul1@gmail.com>
2015-11-13 14:15:45 -0500

    DONE

--- ArrayAnalzyer502/ArrayAnalyzerForm.Designer.cs
+++ ArrayAnalzyer502/ArrayAnalyzerForm.Designer.cs
@@ -29,33 +29,85 @@
         private void InitializeComponent()
         {
             this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.mnuStrip = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnCalculate
             // 
-            this.btnCalculate.Location = new System.Drawing.Point(96, 174);
+            this.btnCalculate.Location = new System.Drawing.Point(113, 76);
             this.btnCalculate.Name = "btnCalculate";
             this.btnCalculate.Size = new System.Drawing.Size(75, 23);
             this.btnCalculate.TabIndex = 0;
-            this.btnCalculate.Text = "button1";
+            this.btnCalculate.Text = "Calculate";
             this.btnCalculate.UseVisualStyleBackColor = true;
             this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(12, 27);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(295, 32);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "Click below to see the greatest number in \r\nan array filled with 10 random number" +
+    "s.";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mnuStrip
+            // 
+            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuStrip.Name = "mnuStrip";
+            this.mnuStrip.Size = new System.Drawing.Size(321, 24);
+            this.mnuStrip.TabIndex = 2;
+            this.mnuStrip.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
             // 
             // frmArray
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(321, 114);
+            this.Controls.Add(this.lblInfo);
             this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.mnuStrip);
+            this.MainMenuStrip = this.mnuStrip;
             this.Name = "frmArray";
-            this.Text = "Form1";
+            this.Text = "Greatest Number";
+            this.mnuStrip.ResumeLayout(false);
+            this.mnuStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.MenuStrip mnuStrip;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
     }
 }
 
--- ArrayAnalzyer502/ArrayAnalyzerForm.cs
+++ ArrayAnalzyer502/ArrayAnalyzerForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Nicholas  Ellul
+ * Created on: 12-Nov-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit 5-02
+ * This program returns the biggest nuumber in an array.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +20,7 @@ namespace ArrayAnalzyer502
 {
     public partial class frmArray : Form
     {
+        //global declaration
         int[] arrayOfNumbers = new int[10];
         Random randomNumbers = new Random();
 
@@ -30,6 +39,7 @@ namespace ArrayAnalzyer502
             return elementHolder;
         }
 
+
         public frmArray()
         {
             InitializeComponent();
@@ -37,10 +47,13 @@ namespace ArrayAnalzyer502
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
+            //input
             const int NUMBER_OF_ITEMS = 10;
             int randomizedNumber;
             int greatestNumber;
-          
+
+            Array.Clear(arrayOfNumbers,0,arrayOfNumbers.Length);
+
             //process
             for (int counter = 0; counter < NUMBER_OF_ITEMS; counter++)
             {
@@ -51,8 +64,13 @@ namespace ArrayAnalzyer502
             //output
             greatestNumber = BiggestNumber(arrayOfNumbers);
 
-            MessageBox.Show(Convert.ToString(greatestNumber));
+            MessageBox.Show(Convert.ToString(greatestNumber), "The greatest number is...");
            
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
